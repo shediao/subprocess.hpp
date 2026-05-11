b9e8bde03d5aab558fdecf10d1b422c136f752ea
shediao.xsd <xushediao1987@163.com>
2026-05-11 17:16:48 +0800

    chore: add MIT license header with ASCII art to subprocess.hpp

--- include/subprocess/subprocess.hpp
+++ include/subprocess/subprocess.hpp
@@ -1,3 +1,42 @@
+/*
+ *               __                                           __
+ *   _______  __/ /_  ____  _________  ________  __________  / /_  ____  ____
+ *  / ___/ / / / __ \/ __ \/ ___/ __ \/ ___/ _ \/ ___/ ___/ / __ \/ __ \/ __ \
+ * (__  ) /_/ / /_/ / /_/ / /  / /_/ / /__/  __(__  |__  ) / / / / /_/ / /_/ /
+ * /____/\__,_/_.___/ .___/_/   \____/\___/\___/____/____(_)_/ /_/ .___/ .___/
+ *                 /_/                                          /_/   /_/
+ *
+ *  subprocess.hpp — A lightweight, header-only C++ subprocess library
+ *                   providing an easy-to-use interface for spawning and
+ *                   managing child processes.
+ *
+ *  Author  : shediao.xsd <xushediao1987@163.com>
+ *  Repo    : https://github.com/shediao/subprocess.hpp
+ *  License : MIT
+ *
+ *  Copyright (c) 2024-2026 shediao.xsd
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ *
+ *  SPDX-License-Identifier: MIT
+ */
+
 #ifndef __SUBPROCESS_SUBPROCESS_HPP__
 #define __SUBPROCESS_SUBPROCESS_HPP__
 
